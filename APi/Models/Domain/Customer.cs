df75ea6e98f6ddf1bb994b604065a828540be906
mohannaddanoun <danoun.mohanad@gmail.com>
2024-12-28 04:06:09 +0300

    update the DB

---
+++ APi/Models/Domain/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace APi.Models.Domain
+{
+    public class Customer
+    {
+        public int Id { get; set; }  
+        public required int NationalId { get; set; } // الرقم الوطني  
+        public required string TaxNumber { get; set; } // الرقم الضريبي
+        public required string CustomerName { get; set; } // اسم العميل
+        public required string RegistrationEntity { get; set; } // جهة التسجيل
+        public required string CompanyType { get; set; } // نوع الشركة
+        public required int  FinancialYear { get; set; } // السنة المالية
+    }
+}
